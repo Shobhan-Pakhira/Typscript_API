00c9cc1f590a4e52c1d310cb8c638d3b9e4eab14
Hacking Devil <shobhanpakhira@gmail.com>
2022-10-06 18:15:35 +0530

    Added author controller and schema

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,10 +5,10 @@ dotenv.config();
 //Creating variable to store confidential information regarding environments | Database Related Information
 const MONGO_USERNAME = process.env.MONGO_USERNAME || '';
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD || '';
-const MONGO_URL = 'mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@ts-api.5sluvep.mongodb.net';
+const MONGO_URL = 'mongodb://localhost:27017';
 
 //Creating variable to store the port number
-const SERVER_PORT = process.env.SERVER_PORT ? Number(process.env.SERVER_PORT) : 8080;
+const SERVER_PORT = process.env.SERVER_PORT ? Number(process.env.SERVER_PORT) : 9090;
 
 //Exporting the config.ts files
 export const config = {
@@ -19,6 +19,3 @@ export const config = {
         port: SERVER_PORT
     }
 };
-
-//shobhanpakhira
-//Sp11062005
--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from 'express'; // importing express package to server.ts
 import http from 'http'; // importing Http package to server.ts
 import mongoose from 'mongoose'; // importing mongoose package to server.ts
 import { config } from './config/config'; // importing config.ts to server.ts
+import Logging from './library/logging';
 
 const router = express();
 
@@ -9,8 +10,57 @@ const router = express();
 mongoose
     .connect(config.mongo.url, { retryWrites: true, w: 'majority' }) //Adding option of mongoose manually
     .then(() => {
-        console.log('Database Connected');
+        Logging.info('Database Connected');
+        StartServer();
     })
     .catch((error) => {
-        console.log(error);
+        Logging.error(error);
     });
+
+//Start Server only if MongoDB Connects
+const StartServer = () => {
+    router.use((req, res, next) => {
+        // Log the request
+        Logging.info(`Incomming - METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}]`);
+
+        res.on('finish', () => {
+            //Log into response
+            Logging.info(`Result - METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}] - STATUS: [${res.statusCode}]`);
+        });
+
+        next();
+    });
+    router.use(express.urlencoded({ extended: true }));
+    router.use(express.json);
+
+    //Rules of API
+    router.use((req, res, next) => {
+        //Allows request from anywhere
+        res.header('Access-Control-Allow-Origin', '*');
+
+        //Type of headers allowed in project
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+        //For returning the option list when an option request is passed
+        if (req.method == 'OPTIONS') {
+            res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+            return res.status(200).json({});
+        }
+        next();
+    });
+
+    //Routes
+
+    //HealthCheck - To Make sure that API is running properly
+    router.get('/ping', (req, res, next) => res.status(200).json({ message: 'Pong' }));
+
+    //Error Handling
+    router.use((req, res, next) => {
+        const error = new Error('Not Found');
+        Logging.error(error);
+
+        return res.status(404).json({ message: error.message });
+    });
+
+    http.createServer(router).listen(config.server.port, () => Logging.info(`Server is running on port ${config.server.port}`));
+};
